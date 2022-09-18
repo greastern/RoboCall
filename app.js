c85d9b107276030965d8f31aeb9f0ce3e7b4926a
Gaius Roy <gaiusroy@gmail.com>
2022-09-17 23:23:27 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const btn = document.querySelector('.talk');
 const content = document.querySelector('.content');
 
+//greetings
+const greetings = [
+    'Im good buddy',
+    'doing good buddy',
+    'i am fine' 
+];
+
+const weather = [
+    'weather is fine',
+    'you need a tan'
+];
+
+
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
 
@@ -13,6 +26,7 @@ recognition.onresult = function(event) {
 
     const transcript = event.results[current][0].transcript;
     content.textContent = transcript;
+    readOutLoud(transcript);
 };
 
 //recognition.onspeechend = function(){
@@ -24,5 +38,22 @@ btn.addEventListener('click', () => {
     recognition.start();
 })
 
+function readOutLoud(message){
+    const speech = new SpeechSynthesisUtterance();
+    
+    speech.text = 'I do not know what you said';
 
+    if(message.includes('how are you')){
+        const finalText = greetings[Math.floor(Math.random() * greetings.length)];
+        speech.text = finalText;
+    }
+    
+    
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+
+    window.speechSynthesis.speak(speech);
+
+}
 
