3bc4554071870b6d8e3c4057b6ca3363b1724d90
Gaius Roy <gaiusroy@gmail.com>
2022-09-17 23:11:36 -0500

    Speech recognition works!

---
+++ app.js
@@ -0,0 +1,28 @@
+const btn = document.querySelector('.talk');
+const content = document.querySelector('.content');
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+recognition.onstart = function() {
+    console.log('voice is activated, you can speak to microphone');
+};
+
+recognition.onresult = function(event) {
+    const current = event.resultIndex;
+
+    const transcript = event.results[current][0].transcript;
+    content.textContent = transcript;
+};
+
+//recognition.onspeechend = function(){
+
+//}
+
+// add listener to button
+btn.addEventListener('click', () => {
+    recognition.start();
+})
+
+
+
